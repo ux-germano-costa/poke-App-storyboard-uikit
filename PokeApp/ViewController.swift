f3b0ae8ccf285ad8116b47013145e502eba57848
GermanoCostaUX <cgermano.fcosta.nnt@gmail.com>
2022-10-07 15:19:54 -0300

    IBAction and IBOutlet commit

--- PokeApp/ViewController.swift
+++ PokeApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var stepperButton: UIStepper!
+    
+    @IBOutlet weak var idLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameLabel.text = "Pokemon"
     }
 
-
+    @IBAction func stepperTouched(_ sender: Any) {
+        print(">>> Stepper Touched <<<")
+    }
+    
 }
 
